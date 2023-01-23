d9e1f74818f47a2b6e6dd9df001545cc04a46427
Viho Akakpo <viheric@gmail.com>
2023-01-23 16:54:15 +0000

    Project is done !

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Timestamp Microservice
 
-This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+This is a completed Timestamp Microservice project. 
+
+Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,13 +18,23 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date?", (req, res) => {
+  let date = /[^0-9]/.test(req.params.date) ? new Date(req.params.date) : new Date(parseInt(req.params.date))
 
+  if (req.params.date === undefined || req.params.date === '' || req.params.date === null) {
+    date = new Date;
+  } else if (isNaN(date.getTime())) {
+    res.json({ error: 'Invalid Date' })
+    return
+  }
+
+  res.json({ utc: date.toUTCString(), unix: parseInt(date.getTime()) })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
